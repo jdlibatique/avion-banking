56f7225e73f51ff7b68b0af2b166e75e447c159d
jbdevelop3r <justinebanogon14@gmail.com>
2022-07-31 00:08:21 +0800

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-
-import LoginForm from './components/LoginForm';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
-            <Routes>
-                <Route exact path={"/"} element={<LoginForm/>}></Route>
-            </Routes>
-        </BrowserRouter>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
