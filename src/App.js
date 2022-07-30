46864cdb3bd21d0922cc26c8c18842dfc53b87c8
Joseph Devereaux Libatique <me@jdlibatique.dev>
2022-07-30 10:54:29 +0800

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-import React from 'react'
-import Homepage from './Pages/Homepage/Homepage';
-import Deposit from './Pages/Deposit/Deposit';
-import Withdraw from './Pages/Withdraw/Withdraw';
-import Transfer from './Pages/Transfer/Transfer';
-import AfterConfirmation from './Pages/AfterConfirmation/AfterConfirmation';
-import SearchAccount from './Pages/SearchAccount/SearchAccount';
+import logo from './logo.svg';
+import './App.css';
 
-
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
-
-function App() { 
-
+function App() {
   return (
-    <div> 
-      <Router>
-        <Routes>
-          <Route path='/Homepage' element={<Homepage/>}/> 
-          <Route path='/Deposit' element={<Deposit/>}/>
-          <Route path='/Withdraw' element={<Withdraw/>}/>
-          <Route path='/Transfer' element={<Transfer/>}/>
-          <Route path='/AfterConfirmation' element={<AfterConfirmation/>}/>
-          <Route path='/SearchAccount' element={<SearchAccount/>}/>
-        </Routes>
-     </Router>
-     
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
-export default App; 
+export default App;
