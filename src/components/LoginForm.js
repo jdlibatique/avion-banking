292e4302f231e75ff6e466e0e754218b421c2757
jbdevelop3r <justinebanogon14@gmail.com>
2022-08-01 20:46:24 +0800

    removed-name

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { useState} from 'react'
 
 function LoginForm({ Login, error}) {
 
-    const [details, setDetails] = useState({name: "", email: "", password: ""});
+    const [details, setDetails] = useState({email: "", password: ""});
 
     const submitHandler = e => {
         e.preventDefault();
@@ -16,8 +16,8 @@ function LoginForm({ Login, error}) {
             <h2>Bankplication <i className="fas fa-sack-dollar"></i></h2>
             {(error != "") ? ( <div className='error'>{error}</div>) : ""}
             <div className="form-group">
-                <label htmlFor="name">Name:</label>
-                <input type="text" name='name' id='name' onChange={e => setDetails({...details, name: e.target.value})} value={details.name}/>
+                {/* <label htmlFor="name">Name:</label>
+                <input type="text" name='name' id='name' onChange={e => setDetails({...details, name: e.target.value})} value={details.name}/> */}
             </div>
             <div className="form-group">
                 <label htmlFor="email">Username:</label>
