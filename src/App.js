85347d6610e6c114addfbaaf4c2707b80a43e193
jbdevelop3r <justinebanogon14@gmail.com>
2022-08-01 20:07:43 +0800

    sample

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState }from 'react'
+import LoginForm from './components/LoginForm';
 
 function App() {
+
+  const adminUser = {
+    username: "admin",
+    password: "admin123"
+  }
+
+  const [user, setUser] = useState({name: "", username: "", password: ""});
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details)
+    if(details.name != "" && details.username == adminUser.username && details.password == adminUser.password) {
+      console.log("Logged In");
+      setUser({
+        name: details.name,
+        username: details.username,
+        password: details.password
+      })
+    } else {
+      setError('Please enter your name')
+    }
+    
+    
+    if (details.username == "" && details.password == "") {
+      setError("Please enter your username and password")
+    } else if (details.username != adminUser.username && details.password != adminUser.password) {
+      setError("Username or Password do not match!")
+    }
+}
+
+  const Logout = () => {
+    console.log("Logout");
+    setUser({ name: "", username: "", password: "" });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.password != "") ? (
+        <div className="welcome">
+          <h2>Welcome, &nbsp; <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error}/>
+        
+      )}
     </div>
   );
 }
