5637c33297541de85bd0ead631e7d011ec8308a0
jbdevelop3r <justinebanogon14@gmail.com>
2022-08-01 20:46:24 +0800

    removed-name

--- src/App.js
+++ src/App.js
@@ -1,47 +1,39 @@
 import React, { useState }from 'react'
 import LoginForm from './components/LoginForm';
 
-function App() {
+function App() { 
 
   const adminUser = {
     username: "admin",
     password: "admin123"
-  }
+  } 
 
-  const [user, setUser] = useState({name: "", username: "", password: ""});
+  const [user, setUser] = useState({username: "", password: ""});
   const [error, setError] = useState("");
 
   const Login = details => {
     console.log(details)
-    if(details.name != "" && details.username == adminUser.username && details.password == adminUser.password) {
+    if(details.username == adminUser.username && details.password == adminUser.password) {
       console.log("Logged In");
       setUser({
-        name: details.name,
         username: details.username,
         password: details.password
       })
-    } else {
-      setError('Please enter your name')
-    }
-    
-    
-    if (details.username == "" && details.password == "") {
-      setError("Please enter your username and password")
     } else if (details.username != adminUser.username && details.password != adminUser.password) {
-      setError("Username or Password do not match!!!")
+      setError("Username or Password do not match!")
     }
 }
 
   const Logout = () => {
     console.log("Logout");
-    setUser({ name: "", username: "", password: "" });
+    setUser({username: "", password: ""});
   }
 
   return (
     <div className="App">
       {(user.password != "") ? (
         <div className="welcome">
-          <h2>Welcome, &nbsp; <span>{user.name}</span></h2>
+          <h2>Welcome, &nbsp; <span>User!</span></h2>
           <button onClick={Logout}>Logout</button>
         </div>
       ) : (
