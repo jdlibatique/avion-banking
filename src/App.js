8cfc403af17a2172451863b5bab342b8b8e728a0
Joseph Devereaux Libatique <me@jdlibatique.dev>
2022-08-02 19:08:12 +0800

    Cleaned up unused code in App.js

--- src/App.js
+++ src/App.js
@@ -2,34 +2,7 @@ import React, { useState }from 'react'
 import LoginForm from './components/LoginForm';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
-function App() { 
-
-//   const adminUser = {
-//     username: "admin",
-//     password: "admin123"
-//   }
-//
-//   const [user, setUser] = useState({username: "", password: ""});
-//   const [error, setError] = useState("");
-//
-//   const Login = details => {
-//     console.log(details)
-//     if(details.username == adminUser.username && details.password == adminUser.password) {
-//       console.log("Logged In");
-//       setUser({
-//         username: details.username,
-//         password: details.password
-//       })
-//     } else if (details.username != adminUser.username && details.password != adminUser.password) {
-//       setError("Username or Password do not match!")
-//     }
-// }
-//
-//   const Logout = () => {
-//     console.log("Logout");
-//     setUser({username: "", password: ""});
-//   }
-
+function App() {
   return (
     <div className="App">
         <BrowserRouter>
@@ -37,15 +10,6 @@ function App() {
                 <Route exact path={"/"} element={<LoginForm/>}></Route>
             </Routes>
         </BrowserRouter>
-      {/*{(user.password != "") ? (*/}
-      {/*  <div className="welcome">*/}
-      {/*    <h2>Welcome, &nbsp; <span>User!</span></h2>*/}
-      {/*    <button onClick={Logout}>Logout</button>*/}
-      {/*  </div>*/}
-      {/*) : (*/}
-      {/*  <LoginForm Login={Login} error={error}/>*/}
-      {/*  */}
-      {/*)}*/}
     </div>
   );
 }
