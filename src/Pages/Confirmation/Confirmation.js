3f1a8cc2e85bc43c23c598588b14c2fa9d0c99f1
jbdevelop3r <justinebanogon14@gmail.com>
2022-08-03 12:13:34 +0800

    pages

---
+++ src/Pages/Confirmation/Confirmation.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Confirmation.css'
+
+function Confirmation() {
+  return (
+    <div className='withdraw-container'>
+        <div className='head-container'>
+            <span>Avion Banking</span>
+            <div className='home-out'>
+                <button className='button-home'>Home</button>
+                <button className='button-logout'>Logout</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Confirmation
