7f09c1e6271112bc5390f50e068dffe42b3921fe
Joseph Devereaux Libatique <me@jdlibatique.dev>
2022-08-03 20:13:55 +0800

    before rebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import LoginForm from "./components/LoginForm";
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
-        
+            <Route path={"/"} element={<LoginForm />}></Route>
         </Routes>
       </BrowserRouter>
   )
