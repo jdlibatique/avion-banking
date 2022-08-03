5cdb641302c8fbbaf940efc0d8048c1aaf089f74
Joseph Devereaux Libatique <me@jdlibatique.dev>
2022-08-03 20:17:09 +0800

    rebasing complete

--- src/App.js
+++ src/App.js
@@ -4,36 +4,23 @@ import Deposit from './Pages/Deposit/Deposit';
 import Withdraw from './Pages/Withdraw/Withdraw';
 import Transfer from './Pages/Transfer/Transfer';
 import Confirmation from './Pages/Confirmation/Confirmation';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, BrowserRouter} from 'react-router-dom'
+import LoginForm from "./components/LoginForm";
 
 
 function App() { 
 
   return (
-    <div className="App">
-      {(user.password != "") ? (
-        <div className="welcome">
-          <h2>Welcome, &nbsp; <span>User!</span></h2>
-          <button onClick={Logout}>Logout</button>
-        </div>
-      ) : (
-        <LoginForm Login={Login} error={error}/>
-        
-      )}
-    <div>
-       
-      <Router>
-        <Routes>
-          <Route path='/Homepage' element={<Homepage/>}/> 
-          <Route path='/Deposit' element={<Deposit/>}/>
-          <Route path='/Withdraw' element={<Withdraw/>}/>
-          <Route path='/Transfer' element={<Transfer/>}/>
-          <Route path='/Confirmation' element={<Confirmation/>}/>
-        </Routes>
-     </Router>
-     
-     </div>
-    </div>
+      <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<LoginForm/>}/>
+            <Route path='/Homepage' element={<Homepage/>}/>
+            <Route path='/Deposit' element={<Deposit/>}/>
+            <Route path='/Withdraw' element={<Withdraw/>}/>
+            <Route path='/Transfer' element={<Transfer/>}/>
+            <Route path='/Confirmation' element={<Confirmation/>}/>
+          </Routes>
+      </BrowserRouter>
   );
 }
 
