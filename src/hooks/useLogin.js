11717a0bd48fb7ab10c56a920f3dda6637711e07
Joseph Devereaux Libatique <me@jdlibatique.dev>
2022-08-04 16:58:12 +0800

    fixed missing dependencies

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -2,11 +2,13 @@ import {useState} from "react";
 import {auth} from "../firebase/config"
 import {signInWithEmailAndPassword} from "@firebase/auth";
 import {useAuthContext} from "./useAuthContext";
+import {useNavigate} from "react-router-dom";
 
 export const useLogin = () => {
     
     const [error, setError] = useState(null);
     const { dispatch } = useAuthContext();
+    const navigate = useNavigate();
     
     const login = (email, password) => {
         setError(null);
@@ -14,6 +16,7 @@ export const useLogin = () => {
             .then((userCredential) => {
                 console.log("Logged in with", userCredential.user.email)
                 dispatch({type: 'LOGIN', payload: userCredential.user})
+                navigate("/Homepage");
         }).catch((error) => {
             setError(error.message);
             console.log(error);
--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,14 +1,17 @@
 import {auth} from "../firebase/config";
 import {signOut} from "@firebase/auth";
 import {useAuthContext} from "./useAuthContext";
+import {useNavigate} from "react-router-dom";
 
 export const useLogout = () => {
     
     const {dispatch} = useAuthContext();
+    const navigate = useNavigate();
     
     const logout = () => {
         signOut(auth).then(() => {
             dispatch({type: 'LOGOUT'});
+            navigate("/");
         }).catch((error) => {
             console.log(error.message);
         })
