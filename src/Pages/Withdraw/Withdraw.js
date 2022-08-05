f0b586e02b3cd2ad72bb0e4117f1bd7f0021b927
Joseph Devereaux Libatique <me@jdlibatique.dev>
2022-08-05 16:04:16 +0800

    Withdrawal working with checking for if balance < amount

--- src/Pages/Withdraw/Withdraw.js
+++ src/Pages/Withdraw/Withdraw.js
@@ -3,13 +3,59 @@ import './Withdraw.css'
 import { useNavigate } from 'react-router-dom'
 import ConfirmationOpen from '../Confirmation/ConfirmationOpen';
 import { useState } from 'react'
+import {doc, getDoc, updateDoc} from "@firebase/firestore";
+import {db} from "../../firebase/config";
+import Swal from "sweetalert2";
 
 
 function Withdraw() {
 
   const navigate = useNavigate();
+    const [accountNumber, setAccountNumber] = useState('');
+    const [amount, setAmount] = useState('');
 
   const [openConfirmation, setOpenConfirmation] = useState(false);
+    
+    const withdrawFromAccount = async () => {
+        const docRef = doc(db, 'accounts', accountNumber);
+        let currentBalance = 0;
+        let nextBalance = 0;
+        console.log(amount, typeof amount)
+        
+        const getCurrentBalance = async (docRef) => {
+            
+            const docSnap = await getDoc(docRef).catch(error =>{
+                Swal.fire(error.message)
+            });
+            
+            if (!docSnap.exists()) {
+                Swal.fire("Oops!", "Account does not exist", `error`)
+                return;
+            }
+            
+            return docSnap.data().balance;
+        }
+        
+        currentBalance = await getCurrentBalance(docRef);
+        
+        console.log("currentBalance: ", currentBalance, typeof currentBalance)
+        if (parseInt(currentBalance) < parseInt(amount)){
+            Swal.fire("Oops!", "Account does not have enough funds!", "error")
+            return;
+        }
+        nextBalance = parseInt(currentBalance) - parseInt(amount)
+        
+        
+        console.log();
+        
+        updateDoc(docRef, {
+            balance: nextBalance
+        })
+            .then(() => {
+                Swal.fire(`Withdrew from Account #${accountNumber}`, `Current Balance in account: ${nextBalance}`, `success`)
+                console.log()
+            })
+    }
 
   return (
     <div className='withdraw-container'>
@@ -21,11 +67,10 @@ function Withdraw() {
             </div>
         </div>
         <div className='withdraw-body'>
-            <input className='button-account' type="text" name="name" placeholder="Account #"></input>
-            <input className='button-amount'  type="text" name="name" placeholder="Amount"></input>
-            <button className="button-withdraw" onClick={() => {setOpenConfirmation(true)}}>Withdraw</button>
+            <input className='button-account' type="text" name="name" placeholder="Account #" onChange={e => setAccountNumber(e.target.value)}></input>
+            <input className='button-amount'  type="text" name="name" placeholder="Amount" onChange={e => setAmount(e.target.value)}></input>
+            <button className="button-withdraw" onClick={withdrawFromAccount}>Withdraw</button>
         </div>
-        {openConfirmation && <ConfirmationOpen closeConfirmation={setOpenConfirmation}/>}
     </div>
   )
 }
