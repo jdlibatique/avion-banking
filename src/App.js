69c29da983c516e29e88e867ce1109689d43330e
Joseph Devereaux Libatique <me@jdlibatique.dev>
2022-08-05 16:18:11 +0800

    moved LoginForm from components to pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Withdraw from './Pages/Withdraw/Withdraw';
 import Transfer from './Pages/Transfer/Transfer';
 import Confirmation from './Pages/Confirmation/Confirmation';
 import {Routes, Route, BrowserRouter, Navigate} from 'react-router-dom'
-import LoginForm from "./components/LoginForm";
+import LoginForm from "./Pages/LoginForm/LoginForm";
 import {useAuthContext} from "./hooks/useAuthContext";
 import SignupForm from "./components/SignupForm";
 import ManageAccount from "./Pages/ManageAccounts/ManageAccount";
