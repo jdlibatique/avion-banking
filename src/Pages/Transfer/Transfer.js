a9d79640bc7242ce34251ff2b44f29727a4604ae
jbdevelop3r <justinebanogon14@gmail.com>
2022-08-05 18:22:20 +0800

    logout

--- src/Pages/Transfer/Transfer.js
+++ src/Pages/Transfer/Transfer.js
@@ -3,20 +3,22 @@ import './Transfer.css'
 import {useNavigate} from 'react-router-dom'
 import ConfirmationOpen from '../Confirmation/ConfirmationOpen';
 import { useState } from 'react'
+import {useLogout} from "../../hooks/useLogout";
 
 function Transfer() {
 
   const navigate = useNavigate();
+  const { logout } = useLogout();
 
   const [openConfirmation, setOpenConfirmation] = useState(false);
 
   return (
     <div className='withdraw-container'>
         <div className='head-container'>
-            <span>Avion Bank</span>
+            <span>Avion Bank</span> 
             <div className='home-out'>
                 <button className='button-home' onClick={() => navigate('/Homepage')}>Home</button>
-                <button className='button-logout'>Logout</button>
+                <button className='button-logout' onClick={logout}>Logout</button>
             </div>
         </div>
         <div className='transfer-body'>
