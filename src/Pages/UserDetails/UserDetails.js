d9c13e0acb020e23ad19f2876df6ab4d3fdca5a4
jbdevelop3r <justinebanogon14@gmail.com>
2022-08-06 10:03:44 +0800

    balance

--- src/Pages/UserDetails/UserDetails.js
+++ src/Pages/UserDetails/UserDetails.js
@@ -4,12 +4,23 @@ import './UserDetails.css'
 function UserDetails() {
   return (
     <>
-    <div className='user-container'>
+    {/* <div className='user-container'>
         <span className='acc-num'>Account #: {}</span>
         <span className='bal'> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Balance:</span>
         <span className='firt-last'>First Name: {} Last Name: {}</span>
         <span className='remain-bal'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{}</span>
-    </div>
+    </div> */}
+
+    <div className='details'>
+          <ul className='top'>
+            <li>Account Number:</li>
+            <li>Balance</li>
+          </ul>
+          <ul className='bottom'>
+            <li>FirstName: {} LastName: {}</li>
+            <li>{}5</li>
+          </ul>
+         </div>
     </>
   )
 }
